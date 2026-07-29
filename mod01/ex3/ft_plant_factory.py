93828965f6bd072c06e8d0edf38e85ba0fba724a
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-07-29 19:06:59 -0300

    initiating plat_factory

---
+++ mod01/ex3/ft_plant_factory.py
@@ -0,0 +1,17 @@
+class Plant:
+	def __init__(
+		self,
+		name: str,
+		starting_height: float,
+		starting_age: int
+	):
+		self.name = name
+		self.starting_height = starting_height
+		self.starting_age = starting_age
+	
+def main() -> None:
+	print("=== Plant Factory Output ===")
+
+
+if __name__ == "__main__":
+	main()

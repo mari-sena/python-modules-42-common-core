d1172e57769e29fc7906b56600c6a9b1c3391d1e
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-08-28 16:14:02 -0300

    Add files via upload

---
+++ mod01/ex6/ft_garden_analytics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+class Plant:
+    def __init__(
+        self,
+        age,
+		height=0.0
+    ):
+        self.age = age
+		self.height = height
+
+
+def main() -> None:
+    print("=== Garden statistics ===")
+	print("=== Check year-old")
+	print("Is 30 days more than a year? -> False")
+	print("Is 400 days more than a year? -> True")
+
+    print()
+	print("Flower")
+
+
+if __name__ = "__main__":
+    main()

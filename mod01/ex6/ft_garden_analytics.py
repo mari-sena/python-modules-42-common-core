27cf048194f9af31ff3e5adebab0917b1f332b02
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-08-28 17:03:57 -0300

    Enhance Flower class with color and statistics
    Added color attribute and statistics method to Flower class.

--- mod01/ex6/ft_garden_analytics.py
+++ mod01/ex6/ft_garden_analytics.py
@@ -3,7 +3,7 @@ class Plant:
     def __init__(
         self,
         name,
-        age,
+        age=0,
         height=0.0
     ):
         self.name = name
@@ -12,17 +12,28 @@ class Plant:
 
     def show(self):
         print(
-            f"{self.name}: {self.height}cm "
+            f"{self.name}: {self.height:.1f}cm, "
             f"{self.age} days old"
         )
 
 
 class Flower(Plant):
-    def __init__(self, name, height=0.0, age=0):
+    def __init__(self, name, height=0.0, age=0, color="unknown"):
         super().__init__(name, age, height)
+        self.color = color
+        self.bloomed = False
 
-    def show():
+    def show(self):
         super().show()
+        print(f" Color: {self.color}")
+        if self.bloomed:
+            print(f" {self.name} is blooming beautifully!")
+        else:
+            print(f" {self.name}: has not bloomed yet")
+	
+    def statistics(self):
+        print(f"[statistics for {self.name}]")
+	
 
 
 def main() -> None:
@@ -33,8 +44,9 @@ def main() -> None:
 
     print()
     print("=== Flower")
-    rose = Flower("Rose", 15.0, 10)
+    rose = Flower("Rose", 15.0, 10, "red")
     rose.show()
+    rose.statistics()
 
 
 if __name__ == "__main__":

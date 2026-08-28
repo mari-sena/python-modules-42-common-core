3ed4db264c94d2da9ec8b859c4eb0382beff45bf
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-08-28 16:41:04 -0300

    Refactor Plant and Flower class constructors and methods

--- mod01/ex6/ft_garden_analytics.py
+++ mod01/ex6/ft_garden_analytics.py
@@ -2,22 +2,40 @@
 class Plant:
     def __init__(
         self,
+        name,
         age,
-		height=0.0
+        height=0.0
     ):
+        self.name = name
         self.age = age
-		self.height = height
+        self.height = height
+
+    def show(self):
+        print(
+            f"{self.name}: {self.height}cm "
+            f"{self.age} days old"
+        )
+
+
+class Flower(Plant):
+    def __init__(self, name, height=0.0, age=0):
+        super().__init__(name, age, height)
+
+    def show():
+        super().show()
 
 
 def main() -> None:
     print("=== Garden statistics ===")
-	print("=== Check year-old")
-	print("Is 30 days more than a year? -> False")
-	print("Is 400 days more than a year? -> True")
+    print("=== Check year-old")
+    print("Is 30 days more than a year? -> False")
+    print("Is 400 days more than a year? -> True")
 
     print()
-	print("Flower")
+    print("=== Flower")
+    rose = Flower("Rose", 15.0, 10)
+    rose.show()
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()

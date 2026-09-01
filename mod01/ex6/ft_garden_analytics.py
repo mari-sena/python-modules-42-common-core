6b2f47fef8c93d72bf722979808c7e62c18b53a8
Mariana Sena <mariana7007@hotmail.com>
2026-09-01 13:57:00 -0300

    fix typos and finish tree return

--- mod01/ex6/ft_garden_analytics.py
+++ mod01/ex6/ft_garden_analytics.py
@@ -21,29 +21,28 @@ class Plant:
         self.height = height
         self._stats = Plant._Stats()
 
-    def show(self):
+    def show(self) -> None:
         self._stats.show += 1
         print(
             f"{self.name}: {self.height:.1f}cm, "
             f"{self.days_old} days old"
         )
 
-    def _display_stats(self):
-        print(f"[statistics for {self.name}]")
+    def _display_stats(self) -> None:
         print(self._stats)
 
-    def grow(self, centimeters: float):
+    def grow(self, centimeters: float) -> None:
         self.height += centimeters
         self._stats.grow += 1
 
 
 class Flower(Plant):
-    def __init__(self, name, height=0.0, days_old=0, color="unknown"):
+    def __init__(self, name: str, height: float = 0.0, days_old: int = 0, color: str = "unknown") -> None:
         super().__init__(name, days_old, height)
         self.color = color
         self.bloomed = False
 
-    def show(self):
+    def show(self) -> None:
         super().show()
         print(f" Color: {self.color}")
         if self.bloomed:
@@ -51,15 +50,37 @@ class Flower(Plant):
         else:
             print(f" {self.name}: has not bloomed yet")
 
-    def bloom(self):
+    def bloom(self) -> None:
         self.bloomed = True
 
 
 class Tree(Plant):
     def __init__(
         self,
-        
-    )
+        name: str,
+        height: float = 0.0,
+        days_old: int = 0,
+        trunk_diameter: float = 0.0
+    ):
+        super().__init__(name, days_old, height)
+        self.trunk_diameter = trunk_diameter
+        self.shade = 0
+
+    def show(self) -> None:
+        super().show()
+        print(f" Trunk diameter: {self.trunk_diameter:.1f}cm")
+
+    def _display_stats(self) -> None:
+        super()._display_stats()
+        print(f" {self.shade} shade")
+
+    def produce_shade(self) -> None:
+        print(
+            f"Tree {self.name} now produces "
+            f"a shade of {self.height:.1f}cm long "
+            f"and {self.trunk_diameter:.1f}cm wide."
+        )
+        self.shade += 1
 
 def main() -> None:
     print("=== Garden statistics ===")
@@ -82,6 +103,12 @@ def main() -> None:
     print("=== Tree")
     oak = Tree("Oak", 200.0, 365, 5.0)
     oak.show()
+    print(f"[statistics for {oak.name}]")
+    oak._display_stats()
+    print("[asking the oak to produce shade]")
+    oak.produce_shade()
+    print(f"[statistics for {oak.name}]")
+    oak._display_stats()
 
 
     print()

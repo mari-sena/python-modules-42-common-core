d02f19e7326d8b1cddb2f4b78f30836e0f0bdee0
Mariana Sena <mariana7007@hotmail.com>
2026-09-04 17:40:24 -0300

    ex3 100%

--- mod3/ex3/ft_achievement_tracker.py
+++ mod3/ex3/ft_achievement_tracker.py
@@ -1,40 +1,30 @@
 import random
 
-achievements: set[str] = [
-    'Crafting Genius',
-    'World Savior',
-    'Master Explorer',
-    'Collector Supreme',
-    'Untouchable',
-    'Boss Slayer',
-    'Strategist',
-    'Unstoppable',
-    'Speed Runner',
-    'Survivor',
-    'Treasure Hunter',
-    'First Steps',
-    'Sharp Mind'
-]
-set_achievements = {
-    'Crafting Genius',
-    'World Savior',
-    'Master Explorer',
-    'Collector Supreme',
-    'Untouchable',
-    'Boss Slayer',
-    'Strategist',
-    'Unstoppable',
-    'Speed Runner',
-    'Survivor',
-    'Treasure Hunter',
-    'First Steps',
-    'Sharp Mind'
-}
+
+def get_achievements() -> list[str]:
+    return [
+        'Crafting Genius',
+        'World Savior',
+        'Master Explorer',
+        'Collector Supreme',
+        'Untouchable',
+        'Boss Slayer',
+        'Strategist',
+        'Unstoppable',
+        'Speed Runner',
+        'Survivor',
+        'Treasure Hunter',
+        'First Steps',
+        'Sharp Mind',
+        'Hidden Path Finder'
+    ]
+
 
 def gen_player_achievements() -> set[str]:
+    achievements = get_achievements()
 
-    return set(random.choices(
-        achievements, k = random.randrange(1, 13)
+    return set(random.sample(
+        achievements, k=random.randrange(1, len(achievements) + 1)
         ))
 
 
@@ -44,30 +34,32 @@ def main() -> None:
     bob = gen_player_achievements()
     charlie = gen_player_achievements()
     dylan = gen_player_achievements()
+    achievements = get_achievements()
 
-    # print(f"\nPlayer Alice: {alice}")
-    # print(f"Player Bob: {bob}")
-    # print(f"Player Charlie: {charlie}")
-    # print(f"Player Dylan: {dylan}")
+    print(f"\nPlayer Alice: {alice}")
+    print(f"Player Bob: {bob}")
+    print(f"Player Charlie: {charlie}")
+    print(f"Player Dylan: {dylan}")
 
-    all_achievements = set()
-    all_achievements.union(achievements)
+    distinct_achievements = alice.union(bob, charlie, dylan)
     print(
         "\nAll distinct achievements: "
-        f"{all_achievements}"
+        f"{distinct_achievements}"
         )
 
-    # print(f"\nCommon achievements: {set_achievements.intersection(alice, bob, charlie, dylan)}")
+    common_achievements = alice.intersection(bob, charlie, dylan)
+    print(f"\nCommon achievements: {common_achievements}")
 
-    # print(f"\nOnly Alice has: {alice.difference(bob, charlie, dylan)}")
-    # print(f"Only Bob has: {bob.difference(alice, charlie, dylan)}")
-    # print(f"Only Charlie has: {charlie.difference(alice, bob, dylan)}")
-    # print(f"Only Dylan has: {dylan.difference(alice, bob, charlie)}")
+    print(f"\nOnly Alice has: {alice.difference(bob, charlie, dylan)}")
+    print(f"Only Bob has: {bob.difference(alice, charlie, dylan)}")
+    print(f"Only Charlie has: {charlie.difference(alice, bob, dylan)}")
+    print(f"Only Dylan has: {dylan.difference(alice, bob, charlie)}")
 
-    # print(f"\nAlice is missing: {set_achievements.difference(alice)}")
-    # print(f"Bob is missing: {set_achievements.difference(bob)}")
-    # print(f"Charlie is missing: {set_achievements.difference(charlie)}")
-    # print(f"Dylan is missing: {set_achievements.difference(dylan)}")
+    set_achievements = set(achievements)
+    print(f"\nAlice is missing: {set_achievements.difference(alice)}")
+    print(f"Bob is missing: {set_achievements.difference(bob)}")
+    print(f"Charlie is missing: {set_achievements.difference(charlie)}")
+    print(f"Dylan is missing: {set_achievements.difference(dylan)}")
 
 
 if __name__ == "__main__":

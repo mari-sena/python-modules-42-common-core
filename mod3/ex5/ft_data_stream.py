24c3b19dd23b88ed5bd251f898d358cd225c6c31
Mariana Sena <mariana7007@hotmail.com>
2026-09-07 12:30:01 -0300

    initiating ex5

---
+++ mod3/ex5/ft_data_stream.py
@@ -0,0 +1,20 @@
+def gen_event():
+    return ()
+
+
+def main() -> None:
+    print("=== Game Data Stream Processor ===")
+    gen_event()
+
+
+if __name__ == "__main__":
+    main()
+
+# next()
+# range()
+# len()
+# print()
+# import typing
+# typing.Generator
+# import random
+# random.*

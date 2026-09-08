f49fff3d435ce0afee549d0a80906b9a6d1fdb44
Mariana Fernandes Guimaraes <mariafer@c3r6p5.42sp.org.br>
2026-09-08 15:40:40 -0300

    fix: needed comprehension on scores

--- mod3/ex6/ft_data_alchemist.py
+++ mod3/ex6/ft_data_alchemist.py
@@ -23,16 +23,21 @@ def main() -> None:
     capitalized_only = [p for p in players if p == p.capitalize()]
     print(f"New list of capitalized names only: {capitalized_only}\n")
 
-    score_dict = dict()
-    for player in capitalized_list:
-        score_dict.update({player: random.randrange(0, 1000)})
+    score_dict = {
+        player: random.randrange(0, 1000)
+        for player in capitalized_only
+    }
     print(f"Score dict: {score_dict}")
 
-    score_list = [s for s in score_dict.values()]
-    score_average = sum(score_list) / len(score_list)
+    score_dict = [s for s in score_dict.values()]
+    score_average = sum(score_dict) / len(score_dict)
     print(f"Score average is {score_average}")
 
-    high_scores = [s for s in score_dict.values() if s > score_average]
+    high_scores = {
+        player: [score_dict]
+        for player in score_dict
+        if score_dict[player] > score_average
+    }
     print(f"High scores: {high_scores}")
 
 

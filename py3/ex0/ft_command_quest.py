bbc6b54905e9891f8c19d0a5ac5135677fe9f26b
Mariana Sena <mariana7007@hotmail.com>
2026-09-10 09:29:36 -0300

    standardizing folder names p3

---
+++ py3/ex0/ft_command_quest.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main() -> None:
+    print("=== Command Quest ===")
+    argslen = len(sys.argv)
+    args = sys.argv[1:]
+    print(f"Program name: {sys.argv[0]}")
+    if argslen == 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {argslen - 1}")
+        for arg in args:
+            print(f"Arguments {i}: {arg}")
+            i += 1
+    print(f"Total arguments: {argslen}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ py3/ex6/ft_data_alchemist.py
@@ -0,0 +1,44 @@
+import random
+
+
+def main() -> None:
+    print("=== Game Data Alchemist ===")
+
+    players = [
+        'Alice',
+        'bob',
+        'Charlie',
+        'dylan',
+        'Emma',
+        'Gregory',
+        'john',
+        'kevin',
+        'Liam'
+    ]
+    print(f"Initial list of players: {players}")
+
+    capitalized_list = [player.capitalize() for player in players]
+    print(f"New list with all names capitalized: {capitalized_list}")
+
+    capitalized_only = [p for p in players if p == p.capitalize()]
+    print(f"New list of capitalized names only: {capitalized_only}\n")
+
+    score_dict = {
+        player: random.randrange(0, 1000)
+        for player in capitalized_only
+    }
+    print(f"Score dict: {score_dict}")
+
+    score_average = sum(score_dict.values()) / len(score_dict)
+    print(f"Score average is {score_average}")
+
+    high_scores = {
+        player: score_dict[player]
+        for player in score_dict
+        if score_dict[player] > score_average
+    }
+    print(f"High scores: {high_scores}")
+
+
+if __name__ == "__main__":
+    main()

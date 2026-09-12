9c1cd6a196c002f647fea6927df67136bfb86731
Mariana Sena <mariana7007@hotmail.com>
2026-09-12 20:08:09 -0300

    ex0 finished

--- py4/ex0/ft_ancient_text.py
+++ py4/ex0/ft_ancient_text.py
@@ -1,23 +1,33 @@
 import sys
+import typing
 
 
 def main() -> None:
-	args_len = len(sys.argv)
-	if args_len == 1:
-		print("Usage: ft_ancient_text.py <file>\n")
-		return
-	if args_len == 2:
-		print("=== Cyber Archives Recovery ===")
-		print(f"Accessing file '{sys.argv[1]}'")
-		try:
-			hapen = open(sys.argv[1])
-			print(hapen)
-		except FileNotFoundError as error:
-			print(
-				f"Error opening file '{sys.argv[1]}': "
-				f"{error}"
-				)
-	
+    args_len = len(sys.argv)
+    if args_len == 1:
+        print("Usage: ft_ancient_text.py <file>\n")
+        return
+    if args_len == 2:
+        filename = sys.argv[1]
+        print("=== Cyber Archives Recovery ===")
+        print(f"Accessing file '{filename}'")
+        try:
+            file: typing.IO[str] = open(filename, "r")
+            content = file.read()
+            print("---\n")
+            print(content)
+            print("\n---")
+            file.close()
+            print(f"File '{filename}' closed.")
+        except PermissionError as error:
+            print(f"Error opening file '{filename}': {error}\n")
+            return
+        except FileNotFoundError as error:
+            print(
+                f"Error opening file '{filename}': "
+                f"{error}\n"
+                )
+
 
 if __name__ == "__main__":
-	main()
+    main()
